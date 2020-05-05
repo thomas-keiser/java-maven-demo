ade7abd72f78436b7f602c58b65fd2c0bece96a7
thomas-keiser <56789214+thomas-keiser@users.noreply.github.com>
2020-05-05 13:40:08 -0400

    Add files via upload

---
+++ src/main/java/com/example/demo/Greeting.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+/**
+ * Created by tlkeise on 4/20/2020.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
